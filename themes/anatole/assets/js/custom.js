b87555887d28b2a11b725b418c2c10fc884dcac6
Alan <ishida327@gmail.com>
2022-07-07 20:48:48 +0900

    added the ghetto mail and the menu

--- themes/anatole/assets/js/custom.js
+++ themes/anatole/assets/js/custom.js
@@ -6,4 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
   var mfc = "©"+year+" J-POP karaoke";
   var main_footer = "<div class='main_custom_footer'>" + mfc + "</div>";
   main.innerHTML += main_footer;
+
+
+  var mail_addr = 'contact@tokyovoicelessons.com'
+  var subject = 'Schedule A Lesson'
+
+  const date = new Date();
+  date.setDate(date.getDate() + 7);
+
+  let month = (1 + date.getMonth()).toString().padStart(2, "0");
+  let day = date.getDate().toString().padStart(2, "0");
+  var day_str = month + "/" + day + "/" + year;
+
+  var body = 'Dear Reina-sensei:\n\n I would like to schedule a lesson for '+ day_str + ' . '
+
+  var mailhref = `mailto:${mail_addr}?subject=${subject}&body=${body}`
+
+  var private = document.querySelector('#pemail');
+  var g2 = document.querySelector('#g2email');
+  var g3 = document.querySelector('#g3email');
+  
+  private.href = mailhref + 'I want a private lesson.'
+  g2.href = mailhref + 'I want a group lesson for 2.'
+  g3.href = mailhref + 'I want a group lesson for 3 or more.'
+
+
 });
