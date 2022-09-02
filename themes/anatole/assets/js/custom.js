9ffb35dabc57cb260de54a00fac64d0f10e1c0ca
Alan <ishida327@gmail.com>
2022-09-02 20:13:34 +0900

    man that aside. sigh

--- themes/anatole/assets/js/custom.js
+++ themes/anatole/assets/js/custom.js
@@ -2,11 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var side_footer = document.querySelector('footer');
   var year = new Date().getFullYear();
   side_footer.innerHTML = "";
-  var main = document.querySelector('main');
-  var mfc = "©"+year+" J-POP karaoke";
-  var main_footer = "<div class='main_custom_footer'>" + mfc + "</div>";
-  main.innerHTML += main_footer;
-
 
   var mail_addr = 'contact@tokyovoicelessons.com'
   var subject = 'Schedule A Lesson'
@@ -18,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let day = date.getDate().toString().padStart(2, "0");
   var day_str = month + "/" + day + "/" + year;
 
-  var body = 'Dear Reina-sensei:\n\n I would like to schedule a lesson for '+ day_str + ' . '
+  var body = 'Dear Rena-sensei:\n\n I would like to schedule a lesson for '+ day_str + ' . '
 
   var mailhref = `mailto:${mail_addr}?subject=${subject}&body=${body}`
 
